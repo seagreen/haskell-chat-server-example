fdd4ac48d2c7fa29553e3c3ac087298ca2c7c537
Ian Grant Jeffries <ian@housejeffries.com>
2020-01-27 18:02:22 -0500

    Update to GHC 8.6.5
    Add `network-simple` as a new dependency, because `network` has deprecated
    some of the functions we were using and become more low-level.

--- Main.hs
+++ Main.hs
@@ -2,22 +2,38 @@ module Main where
 
 import Control.Concurrent (forkFinally)
 import Control.Monad      (forever)
-import Network            (PortID(..), accept, listenOn, withSocketsDo)
-import System.IO          (hClose)
+import Network.Socket     (socketToHandle)
+import Network.Simple.TCP (HostPreference(Host), serve)
+import System.IO          (IOMode(ReadWriteMode), hClose)
 import Text.Printf        (printf)
 
 import Server             (handleClient, newServer)
 
 main :: IO ()
-main = withSocketsDo $ do
+main = do
     server <- newServer
-    sock <- listenOn (PortNumber (fromIntegral port))
     _ <- printf "Listening on port %d\n" port
-
-    forever $ do
-        (handle, host, port') <- accept sock
-        _ <- printf "Accepted connection from %s: %s\n" host (show port')
-        forkFinally (handleClient handle server) (\_ -> hClose handle)
+    serve (Host "127.0.0.1") (show port) $ \(connectionSocket, remoteAddr) -> do
+        _ <- printf "Accepted connection from %s\n" (show remoteAddr)
+        handle <- socketToHandle connectionSocket ReadWriteMode
+        handleClient handle server
+        pure ()
 
 port :: Int
 port = 44444
+
+-- The `network` became more low-level over time, so this has changed
+-- from the previous code and now uses `network-simple` as well.
+-- The original code is below:
+--
+--
+-- main :: IO ()
+-- main = withSocketsDo $ do
+--     server <- newServer
+--     sock <- listenOn (PortNumber (fromIntegral port))
+--     _ <- printf "Listening on port %d\n" port
+--
+--     forever $ do
+--         (handle, host, port') <- accept sock
+--         _ <- printf "Accepted connection from %s: %s\n" host (show port')
+--         forkFinally (handleClient handle server) (\_ -> hClose handle)
